e3dc9ad1feddaf2cc51730e982ab45383e82d49c
Leon Graff <442grafF@gmail.com>
2025-05-18 14:28:40 -0300

    first commit

---
+++ js/routes.js
@@ -0,0 +1,46 @@
+const routes = {
+    "/sobre": "pages/sobre.html",
+    "/formacao": "pages/formacao.html",
+    "/portfolio": "pages/portfolio.html",
+    "/contato": "pages/contato.html",
+};
+
+async function loadPage() {
+    let path = location.hash.slice(1);
+
+    if (!path || path === "/") {
+        location.hash = "#/sobre";
+        return;
+    }
+
+    const page = routes[path];
+
+    if (!page) {
+        document.getElementById("render-div").innerHTML = "<h1>404 - Página não encontrada</h1>";
+        return;
+    }
+    try {
+        const res = await fetch(page);
+        const html = await res.text();
+        document.getElementById("render-div").innerHTML = html;
+        updateActiveLink();
+    } catch (err) {
+        document.getElementById("render-div").innerHTML = "<h1>Erro ao carregar a página.</h1>";
+    }
+}
+
+window.addEventListener("hashchange", loadPage);
+window.addEventListener("load", loadPage);
+
+function updateActiveLink() {
+    const currentPath = location.hash;
+    const navLinks = document.querySelectorAll("nav a");
+
+    navLinks.forEach((link) => {
+        if (link.getAttribute("href") === currentPath) {
+            link.classList.add("active");
+        } else {
+            link.classList.remove("active");
+        }
+    });
+}
